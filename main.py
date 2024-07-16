7930e98308849bda0e74127eec8f25c4ece82fdd
Saptarshi Sanyal <saptarshi.edp@gmail.com>
2024-07-16 10:45:58 +0530

    Completed Data Transformation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.entity import config_entity
 from src.components.data_ingestion import DataIngestion
 from src.logger import logging
 from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
 
 
 
@@ -25,16 +26,12 @@ if __name__=="__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
 
           data_validation_artifact = data_validation.initiate_data_validation()
-     except Exception as e:
-          print(e)
 
-
-if __name__=="__main__":
-     try:
-          training_pipeline_config = config_entity.TrainingPipelineConfig()
-          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-          print(data_ingestion_config.to_dict())
-          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+           #data transformation
+          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+          data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
      except Exception as e:
-          print(e)
+          raise SensorException(e,sys)
+
