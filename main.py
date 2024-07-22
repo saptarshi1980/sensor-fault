834501ec26e16780bc399c23498fcee037f2060d
Saptarshi Sanyal <saptarshi.edp@gmail.com>
2024-07-22 14:09:36 +0530

    Completed Model Evaluation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.logger import logging
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
-
+from src.components.model_evaluation import ModelEvaluation
 
 
 
@@ -39,7 +39,16 @@ if __name__=="__main__":
           #model trainer
           model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
           model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
-          model_trainer_artifact = model_trainer.initiate_model_trainer()     
+          model_trainer_artifact = model_trainer.initiate_model_trainer()  
+
+          model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+           data_ingestion_artifact=data_ingestion_artifact,
+           data_transformation_artifact=data_transformation_artifact,
+            model_trainer_artifact=model_trainer_artifact)
+          model_eval_artifact = model_eval.initiate_model_evaluation()
+
+
           
      except Exception as e:
           raise SensorException(e,sys)
